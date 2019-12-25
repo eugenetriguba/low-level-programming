55108a3a00b9936239b85aa20ba5a003220b4a7e
Eugene Triguba <eugenetriguba@gmail.com>
2019-12-24 21:22:27 -0600

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# @file Enviroment Setup
+# @brief Bootstrap script for installing tools used. Assumes apt (Ubuntu package manager) is available.
+
+sudo apt install nasm gcc make gdb
