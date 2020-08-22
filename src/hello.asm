a3545bc6d10cf838d2c4c17dcd6f51e6bb0e5da9
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-22 14:37:51 -0500

    Revert file format for hello

--- src/hello.asm
+++ src/hello.asm
@@ -2,18 +2,19 @@
 ; 
 ; Prints out 'Hello, world!' to stdout.
 
-           global    _start
+global _start
 
-           section   .data
-message:   db        'Hello, world!', 10  ; followed by a newline.
+section .data
+message: db 'Hello, world!', 10  ; followed by a newline.
 
-           section   .text
-_start:    mov       rax, 1       ; write syscall number
-           mov       rdi, 1       ; stdout file descriptor
-           mov       rsi, message ; string address
-           mov       rdx, 14      ; string length in bytes
-           syscall
+section .text
+_start:
+mov rax, 1       ; write syscall number
+mov rdi, 1       ; stdout file descriptor
+mov rsi, message ; string address
+mov rdx, 14      ; string length in bytes
+syscall
 
-           mov       rax, 60      ; exit syscall number
-           xor       rdi, rdi
-           syscall
+mov rax, 60      ; exit syscall number
+xor rdi, rdi
+syscall
