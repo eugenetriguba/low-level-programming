312e2398fb2776b94e12b45d873834319e552791
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-22 14:29:08 -0500

    Lint hello asm file

--- src/hello.asm
+++ src/hello.asm
@@ -2,19 +2,18 @@
 ; 
 ; Prints out 'Hello, world!' to stdout.
 
-section .data
-message: db 'Hello, world!', 10  ; our message, followed by a newline.
+           global    _start
 
-section .text
-global _start
+           section   .data
+message:   db        'Hello, world!', 10  ; followed by a newline.
 
-_start:
-    mov rax, 1       ; write syscall number
-    mov rdi, 1       ; stdout file descriptor
-    mov rsi, message ; string address
-    mov rdx, 14      ; string length in bytes
-    syscall
+           section   .text
+_start:    mov       rax, 1       ; write syscall number
+           mov       rdi, 1       ; stdout file descriptor
+           mov       rsi, message ; string address
+           mov       rdx, 14      ; string length in bytes
+           syscall
 
-    mov rax, 60      ; exit syscall number
-    xor rdi, rdi
-    syscall
+           mov       rax, 60      ; exit syscall number
+           xor       rdi, rdi
+           syscall
