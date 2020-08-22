56a5d32b53a2673d6b28ddc894d0a127edeedcb8
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-22 16:04:58 -0500

    Add newline to end of print rax

--- src/hello.asm
+++ src/hello.asm
@@ -5,16 +5,17 @@
 global _start
 
 section .data
-message: db 'Hello, world!', 10  ; followed by a newline.
+message:
+    db 'Hello, world!', 10  ; followed by a newline.
 
 section .text
 _start:
-mov rax, 1       ; write syscall number
-mov rdi, 1       ; stdout file descriptor
-mov rsi, message ; string address
-mov rdx, 14      ; string length in bytes
-syscall
+    mov rax, 1       ; write syscall number
+    mov rdi, 1       ; stdout file descriptor
+    mov rsi, message ; string address
+    mov rdx, 14      ; string length in bytes
+    syscall
 
-mov rax, 60      ; exit syscall number
-xor rdi, rdi
-syscall
+    mov rax, 60      ; exit syscall number
+    xor rdi, rdi
+    syscall
--- src/print_rax.asm
+++ src/print_rax.asm
@@ -1,32 +1,46 @@
+; print_rax.asm
+;
+; Print out the rax register in hexadecimal.
+
+global _start
+
 section .data
-codes:
-db      '0123456789ABCDEF'
+hex_value: dq 0x1122334455667788
+hex_codes: db '0123456789ABCDEF'
+newline:   db 10
 
 section .text
-global _start
+
 _start:
-mov rax, 0x1122334455667788
+    mov rax, [hex_value]  ; Store the memory contents at hex_value into rax
 
-mov rdi, 1
-mov rdx, 1
-mov rcx, 64
-.loop:
-push rax
-sub rcx, 4
-sar rax, cl
-and rax, 0xf
+    mov rdi, 1
+    mov rdx, 1
+    mov rcx, 64
 
-lea rsi, [codes + rax]
-mov rax, 1
+    .loop:
+        push rax
+        sub rcx, 4
+        sar rax, cl
+        and rax, 0xf
 
-push rcx
-syscall
-pop rcx
-pop rax
-test rcx, rcx
-jnz .loop
+        lea rsi, [hex_codes + rax]
+        mov rax, 1
 
-mov rax, 60             ; invoke 'close' system call
+        push rcx
+        syscall
+        pop rcx
 
-xor rdi, rdi
-syscall
+        pop rax
+        test rcx, rcx
+        jnz .loop
+
+    mov rax, 1       ; write syscall number
+    mov rdi, 1       ; stdout file descriptor
+    mov rsi, newline ; string address of newline
+    mov rdx, 1       ; string length in bytes
+    syscall
+
+    mov rax, 60   ; exit system call
+    xor rdi, rdi  ; 
+    syscall
